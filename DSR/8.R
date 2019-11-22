92156f4cbb3b6c5faf45fa7459e5b98f54a9c137
harinibm <50053291+harinibm@users.noreply.github.com>
2019-11-22 08:23:42 +0530

    Update 8.R

--- DSR/8.R
+++ DSR/8.R
@@ -1,23 +1,46 @@
-install.packages("e1071")
-library(e1071)
-setwd("C:/users/deepthi")
-grep(".csv",list.files(),value = T)
-b<-read.csv("samplebayes.csv")
-b
-class(b)
-testset<-data.frame(Age="<=30",Income="Medium",JobSatisfaction="No",Desire="Fair",Enrolls="")
-testset
-b<-rbind(b,testset)
-b
-traindata <- as.data.frame(b[1:14,])
-testdata <- as.data.frame(b[15,])
-traindata
-testdata
-bayesmodel <- naiveBayes(Enrolls ~ Age+Income+JobSatisfaction+Desire,traindata)
-bayesmodel
-results <- predict (bayesmodel,testdata)
-results
-modell = naiveBayes(Enrolls ~ ., traindata, laplace=.01)
-modell
-results1<-predict(modell,testdata)
-results1
+install.packages("e1071")
+
+library(e1071)
+
+setwd("C:/users/harinibm")
+
+grep(".csv",list.files(),value = T)
+
+b<-read.csv("samplebayes.csv")
+
+b
+
+class(b)
+
+testset<-data.frame(Age="<=30",Income="Medium",JobSatisfaction="No",Desire="Fair",Enrolls="")
+
+testset
+
+b<-rbind(b,testset)
+
+b
+
+traindata <- as.data.frame(b[1:14,])
+
+testdata <- as.data.frame(b[15,])
+
+traindata
+
+testdata
+
+bayesmodel <- naiveBayes(Enrolls ~ Age+Income+JobSatisfaction+Desire,traindata)
+
+bayesmodel
+
+results <- predict (bayesmodel,testdata)
+
+results
+
+modell = naiveBayes(Enrolls ~ ., traindata, laplace=.01)
+
+modell
+
+results1<-predict(modell,testdata)
+
+results1
+
